9deca4ee4b5fe2822dc2c1994f41d0db2db2a18c
Tyler Mangan <mangantj@gmail.com>
2017-07-12 11:02:41 -0500

    Remove params setting redirect_uri. Stops redirect_uri being set by omniauth

--- lib/omniauth/strategies/emma.rb
+++ lib/omniauth/strategies/emma.rb
@@ -21,7 +21,6 @@ module OmniAuth
         super.tap do |params|
           params[:response_type] ||= DEFAULT_RESPONSE_TYPE
           params[:client_id] = client.id
-          params[:redirect_uri] = options.client_redirect_uri
         end
       end
 
